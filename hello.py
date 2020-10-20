22222ffab0897184dfdfc998bedd3e481b002859
Vicky Kumar <imvickykumar999@gmail.com>
2020-10-20 10:50:40 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+app = Flask(__name__) 
+  
+# routing the decorator function hello_name 
+@app.route('/hello/<name>')   
+def hello_name(name): 
+   return 'Hello %s!' % name 
+  
+if __name__ == '__main__': 
+   app.run(debug = True)
+
+# http://localhost:5000/hello/Vicky%20Kumar
